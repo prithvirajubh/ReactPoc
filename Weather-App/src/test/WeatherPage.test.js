00050ac7df2ea0fac40edc0193204df1ad7d7efc
Prithvi raju <prithviraju.br@tadigital.com>
2024-03-22 16:37:03 +0530

    changes

--- Weather-App/src/test/WeatherPage.test.js
+++ Weather-App/src/test/WeatherPage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect"; // for additional matchers like .toBeInTheDocument()
+import "@testing-library/jest-dom/extend-expect";
 import App from "../page/WeatherPage/index";
 
 describe("App component", () => {
--- Weather-App/src/test/cityInput.test.js
+++ Weather-App/src/test/cityInput.test.js
@@ -1,56 +1,6 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import CityInput from '../components/Input/index';
-
-// describe('CityInput component', () => {
-//     let wrapper;
-//     const mockApiCall = jest.fn();
-
-//     beforeEach(() => {
-//         wrapper = shallow(<CityInput ApiCall={mockApiCall} />);
-//     });
-
-//     afterEach(() => {
-//         jest.clearAllMocks();
-//     });
-
-//     it('renders input field with placeholder', () => {
-//         expect(wrapper.find('.city-input').exists()).toBe(true);
-//         expect(wrapper.find('.city-input').prop('placeholder')).toEqual('Enter a City...');
-//     });
-
-//     it('calls ApiCall function on pressing enter key', () => {
-//         const eventMock = {
-//             persist: jest.fn(),
-//             target: { value: 'New York' },
-//             which: 13,
-//             preventDefault: jest.fn(),
-//             keyCode: 13,
-//         };
-
-//         wrapper.find('.city-input').simulate('keypress', eventMock);
-//         expect(mockApiCall).toHaveBeenCalledWith('New York');
-//     });
-
-//     it('displays correct placeholder text on invalid city name', () => {
-//         const eventMock = {
-//             persist: jest.fn(),
-//             target: { value: 'InvalidCity123' },
-//             which: 13,
-//             preventDefault: jest.fn(),
-//             keyCode: 13,
-//         };
-
-//         wrapper.find('.city-input').simulate('keypress', eventMock);
-//         expect(wrapper.find('.city-input').prop('placeholder')).toEqual('Please enter a valid city name...');
-//     });
-
-//     // Add more test cases as needed...
-// });
-
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom'; // To use Jest DOM matchers
+import '@testing-library/jest-dom';
 
 import CityInput from '../components/Input/index';
 
